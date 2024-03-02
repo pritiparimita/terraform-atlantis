669f6858355e2d86d3587a6c8b2476278509a569
pritiparimita <148382527+pritiparimita@users.noreply.github.com>
2024-03-02 09:58:25 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "<S3-BUCKET-NAME>"
+    key = "atlantis/terraform.tfstate"
+    region = "ap-south-1"
+    profile = "default"
+  }
+}
+resource "aws_instance" "this" {
+  ami = "ami-"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "$WORKSPACE"
+  }
+}
